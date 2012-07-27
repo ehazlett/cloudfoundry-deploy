6e2cbf06292611732bc69e5563ea90ca043ac958
Evan Hazlett <ejhazlett@gmail.com>
2012-07-27 01:38:30 -0500

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+#
+# Arcus CloudFoundry installation
+#  (c) arcus.io 2012
 
+# check for root
 if [ "$UID" != "0" ]; then
     echo "You must be root to install..."
     exit 1
@@ -10,23 +14,31 @@ echo " Arcus CloudFoundry Install"
 echo "---------------------------"
 echo ""
 
+# check for git
 if [ ! -e `which git` ] ; then
     echo "You must have git installed.  On ubuntu or debian, you can apt-get install git-core"
     exit 1
 fi
+# check for args
 if [ "$1" == "" ]; then
     echo "Usage: $0 <DEPLOY_TYPE> <NATS_HOST> <DOMAIN>\n\nWhere DEPLOY_TYPE is controller-<index>, db-<index>, or dea-<index> (i.e. db-0)\nand NATS_HOST is the NATS MB host and DOMAIN is the custom domain (leave blank for vcap.me)\n"
     exit 1
 fi
-
+# edit configs
 sed -i '' "s/NATS_HOST/$2/g" $1.yml
 
+# check for vcap
+if [ ! -e "~/vcap" ] ; then
+    git clone https://github.com/cloudfoundry/vcap.git ~/vcap
+fi
+# install
 ~/vcap/dev_setup/bin/vcap_dev_setup -a -c $1.yml -D $3
+
 if [ "$?" != "0" ]; then
     echo "Error during CloudFoundry installation"
     exit 1
 fi
-
+# copy management script
 cp cf_control.sh /usr/local/bin/cf_control
 chmod +x /usr/local/bin/cf_control
 
