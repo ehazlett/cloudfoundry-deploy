70ccf93110d48d697d6346acd807860dba25bb29
Evan Hazlett <ejhazlett@gmail.com>
2012-12-06 14:08:51 -0500

    updated controller vcap components ; updated cf_control

--- cf_control.sh
+++ cf_control.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-if [ "$1" = "" ] ; then echo "Usage: `basename $0` [action]"; exit 1; fi
-$HOME/cloudfoundry/vcap/dev_setup/bin/vcap_dev -n `ls ~/cloudfoundry/.deployments` $1
+if [ "$1" = "" ] ; then echo "Usage: `basename $0` <action> [service]"; exit 1; fi
+$HOME/cloudfoundry/vcap/dev_setup/bin/vcap_dev -n `ls ~/cloudfoundry/.deployments` $1 $2
