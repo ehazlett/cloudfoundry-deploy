a995cd56e29e7c0a7a2e733e61ca098ce22e0114
Evan Hazlett <ejhazlett@gmail.com>
2012-12-16 03:07:42 -0500

    updated set_role

--- set_role.sh
+++ set_role.sh
@@ -20,4 +20,19 @@ if [ "$1" = "" ]; then
 fi
 # copy vcap components
 log " Configuring for $1"
+
+if [ "$1" != "controller" ]; then
+    # disable controller services
+    sudo update-rc.d -f nginx_cc remove
+    sudo update-rc.d -f nginx_sds remove
+    sudo update-rc.d -f nginx_router remove
+    sudo update-rc.d -f nats_server remove
+    sudo update-rc.d -f vcap_redis remove
+
+    sudo service nginx_cc stop
+    sudo service nginx_sds stop
+    sudo service nginx_router stop
+    sudo service nats_server stop
+    sudo service vcap_redis stop
+fi
 cp -f $PROJECT_DIR/vcap_configs/$1_vcap_components.json $HOME/cloudfoundry/.deployments/vcap/config/vcap_components.json
